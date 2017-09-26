0b96bca0a9d4fcd8fe00726caa7537eedbeff86d
noelpei0461 <31324311+noelpei0461@users.noreply.github.com>
2017-09-26 10:34:20 +0800

    Update inclass8.m

--- inclass8.m
+++ inclass8.m
@@ -27,11 +27,7 @@ ans =
 seq1='CACGATGCCACCTCATCCCGACCGCGTGAGATAACGTCATTCCATTGGCTGGAATCTGACTGGCCGAATCTCACTATGTTCGGCTCCTGAAGCCAGTATA'
 % B. Create a second sequence from the first one by changing the middle 20 basepairs (at positions 41-60)
 
-randdnaseq(20)
-
-ans =
-
-    'CTGAGTGGGTAACACGCATA' 
+seq2 = [seq1(1:40) randseq(20) seq1(60:100)];
 seq2='CACGATGCCACCTCATCCCGACCGCGTGAGATAACGTCATCTGAGTGGGTAACACGCATATGGCCGAATCTCACTATGTTCGGCTCCTGAAGCCAGTATA'    
 % C. run swalign on the two sequences with the default parameters
 figure;
@@ -39,7 +35,7 @@ figure;
 % D. run swalign with much higher and lower values of the GapOpen parameter
 % and explain the results. 
 
-%Gap opening parameter refers to the cost that a open a gap make take. The higher the cost is, the less possible to have a gap in the alignment. Thus the figure with lower Gapopen parameter twist more.
+%Gap opening parameter refers to the cost that a open a gap make take. The higher the cost is, the less possible to have a great gap in the alignment. Thus the figure with higher Gapopen parameter becomes more continurosly.
 
 
 % E. run swalign with a low value of GapOpen but a high value of ExtendGap,
@@ -49,10 +45,11 @@ start =
 
      1
      1
-%Although the gapopen penalty is low, the cost for extending the alignment is too high, that the alignment stops at the first pair.
+%Although the gapopen penalty is low, the cost for extending the alignment is too high, that the alignment gap can occur in the first pair(41), but cannot extend in the later parts.
 % F. run swalign align with the default value of GapOpen and a low value
 % of ExtendGap and explain the result. 
 figure;
 [score,align,start]=swalign(seq1,seq2,'Alphabet','nt','ExtendGap',1,'Showscore',true)
 
 
+% Since the gap extending penalty is low, the greater gap can occur in the graph.
